cd02b0166f9037daa3949deb456a8004807f6ec4
Nicoli Lisboa <nick.lisboa04@gmail.com>
2025-08-21 13:35:34 -0300

    Deploy

--- src/core/database.py
+++ src/core/database.py
@@ -4,8 +4,7 @@ import os
 import sqlite3
 
 DB_PATH = os.environ.get("DB_PATH", "./data/sqlite3.db")
-os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
-
+# os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
 
 def _execute(query, params=None):
     connection = sqlite3.connect(DB_PATH)
